fef6b20963d5e3fefc822911e31ed9b6ea01fc4a
RagnvaldArnulfson <gus683@hotmail.fr>
2016-04-25 23:07:43 +0200

    Update 2-players.py

--- 2-players.py
+++ 2-players.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from threading import Thread
 import random as r
 import collections as c
 
@@ -32,12 +33,12 @@ def playGame(state, verbose):
     played = []
     game = []
     while state[0] != [] and state[1] != [] and [state,played] not in game:
-        game = game+[[deepcopy(state),played[::]]]
+        game = game+deepcopy([[state,played]])
         state, played = playTurn(state, played)
     if [state,played] in game:
         print("INFINITE")
         print(game)
-        return "wow"
+        return ["wow",game]
     elif verbose and played != []:
         print("Draw")
     elif verbose and len(state[0]) > len(state[1]):
@@ -45,7 +46,65 @@ def playGame(state, verbose):
     elif verbose:
         print("Player 2 win")
     return game + [state, played]
+    
+def statesBefore(current_state, played):
+    if played != []:
+        hist = [played[0]]
+        return [[[hist+current_state[0], hist+current_state[1]],played[2:]]]
+    before = []
+    for i in range(2):
+        len_deck = len(current_state[i])
+        if len_deck > 1:
+            a = current_state[i][len_deck-1]
+            b = current_state[i][len_deck-2]
+            if a < b:
+                before += [[[[a]+current_state[1-i],[b]+current_state[i][:len_deck-2]],[]]]
+            elif a == b:
+                before += [[[current_state[1-i],current_state[i][:len_deck-2]],[a,a]]]
+    return before
 
-lol = distribute(shuffleDeck(newDeck(3,4)))
-while playGame(lol,True) != "wow":
-    lol = distribute(shuffleDeck(newDeck(13,4)))
+def findGame(state, knownGame):
+    if len(state[0][0]) == len(state[0][1]):
+        return knownGame
+    
+    for hypothesis in statesBefore(state[0],state[1]):
+        return findGame(hypothesis, [hypothesis]+knownGame)
+    
+    
+def randomSearch(n,c):
+    lol = distribute(shuffleDeck(newDeck(n,c)))
+    mdr = playGame(lol,False)
+    j = 0
+    while mdr[0]!="wow":
+        j+=1
+        if(j%100==0):
+            print(j)
+        lol = distribute(shuffleDeck(newDeck(n,c)))
+        mdr = playGame(lol,False)
+    return mdr[1]
+            
+class obj_RandomSearch(Thread):
+    def __init__(self, n, c):
+        Thread.__init__(self)
+        self.n = n
+        self.c = c
+        self.fichier = open("infini"+str(na)+"_"+str(ca)+"-"+str(r.randint(111111,999999))+".txt", "w")
+
+    def run(self):
+        self.fichier.write(str(randomSearch(self.n,self.c)))
+        self.fichier.close()
+        
+def foo():
+    i = 0
+    na, ca = int(input("n?")),int(input("c?"))
+    threadList = []
+    
+    for i in range(10) :
+        curThread = obj_RandomSearch(na,ca)
+        curThread.daemon = True
+        curThread.start()
+        threadList.append(curThread)
+    
+    for curThread in threadList :
+    
+        curThread.join()
